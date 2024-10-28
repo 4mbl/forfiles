189ee31b9be6ac7c9e4cae90567926d90ce9f9d9
4MBL <74970679+4mbl@users.noreply.github.com>
2024-10-28 15:06:09 +0200

    Python 3.13 support (#2)
    * remove deprecated functions
    
    * upgrade pillow
    
    * drop python 3.8 and 3.7 support
    
    * remove vscode settings
    
    * remove `print_paths` argument from `dir_action` function to avoid non-keyword boolean argument
    
    * remove layered image support in `to_png` function to mimimize deps to just pillow
    
    * remove examples from source files
    
    * update classifiers
    
    * create publish workflow

--- src/forfiles/image.py
+++ src/forfiles/image.py
@@ -1,13 +1,18 @@
 import os
-from typing_extensions import deprecated
 from PIL import Image
-import layeredimage as layered_image  # type: ignore
 from .directory import dir_action
 
-IMAGE_TYPES = (".png", ".jpg", ".gif", ".webp", ".tiff", ".bmp", ".jpe",
-               ".jfif", ".jif")
-
-LAYERED_IMAGE_TYPES = (".ora", ".pdn", ".xcf", ".psd")
+IMAGE_TYPES = (
+    ".png",
+    ".jpg",
+    ".gif",
+    ".webp",
+    ".tiff",
+    ".bmp",
+    ".jpe",
+    ".jfif",
+    ".jif",
+)
 
 
 def resize(path: str, image_width: int, image_height: int):
@@ -36,7 +41,7 @@ def resize(path: str, image_width: int, image_height: int):
         resize_single(path)
 
     if os.path.isdir(path):
-        dir_action(path, True, resize_single)
+        dir_action(path, resize_single)
 
 
 def scale(path: str, width_multiplier: float, height_multiplier: float):
@@ -58,8 +63,10 @@ def scale(path: str, width_multiplier: float, height_multiplier: float):
                 image_width, image_height = image.size
 
                 image = image.resize(
-                    (int(image_width * width_multiplier),
-                     int(image_height * height_multiplier)),
+                    (
+                        int(image_width * width_multiplier),
+                        int(image_height * height_multiplier),
+                    ),
                     resample=Image.Resampling.NEAREST,
                 )
                 image.save(path)
@@ -68,45 +75,11 @@ def scale(path: str, width_multiplier: float, height_multiplier: float):
         scale_single(path)
 
     if os.path.isdir(path):
-        dir_action(path, True, scale_single)
-
-
-@deprecated("Use `directory.dir_action` instead")
-def dir_scale(dir_path: str, width_multiplier: float, height_multiplier: float):
-    """
-    Scales every image in a directory and its sub directories.
-
-    Args:
-        dir_path (str): path of the directory that will be used
-        width_multiplier (int): width of all images is multiplied by this
-        height_multiplier (int): height of all images is multiplied by this
-    """
-
-    for root, _, files in os.walk(dir_path):
-        for file in files:
-            print(os.path.join(root, file).replace("\\", "/"))
-            scale(os.path.join(root, file), width_multiplier, height_multiplier)
-
-
-@deprecated("Use `directory.dir_action` instead")
-def dir_resize(dir_path: str, image_width: int, image_height: int):
-    """
-    Resizes every image in a directory and its sub directories.
-
-    Args:
-        dir_path (str): path of the directory that will be used
-        width_multiplier (int): width of the desired output image in pixels
-        height_multiplier (int): height of the desired output image in pixels
-    """
-
-    for root, _, files in os.walk(dir_path):
-        for file in files:
-            print(os.path.join(root, file).replace("\\", "/"))
-            resize(os.path.join(root, file), image_width, image_height)
+        dir_action(path, scale_single)
 
 
 def to_png(path: str):
-    """Converts normal image or layered image file into PNG.
+    """Converts an image file into PNG.
 
     Args:
         path (str): path of the layered image to convert or to a directory that contains them
@@ -116,10 +89,7 @@ def to_png(path: str):
         if path.endswith(".png"):
             return
         filename = os.path.splitext(path)[0]
-        if path.endswith(LAYERED_IMAGE_TYPES):
-            image = layered_image.openLayerImage(path)
-            image.getFlattenLayers().save(f"{filename}.png")
-        elif path.endswith(IMAGE_TYPES):
+        if path.endswith(IMAGE_TYPES):
             image = Image.open(path)
             image.save(f"{filename}.png")
             os.remove(path)
@@ -128,32 +98,4 @@ def to_png(path: str):
         to_png_single(path)
 
     if os.path.isdir(path):
-        dir_action(path, True, to_png_single)
-
-
-@deprecated("Use `directory.dir_action` instead")
-def dir_to_png(dir_path: str):
-    """
-    Converts every image or layered image file in a directory and its sub directories into PNG.
-
-    Args:
-        dir_path (str): path of the directory that will be used
-    """
-
-    for root, _, files in os.walk(dir_path):
-        for file in files:
-            print(os.path.join(root, file).replace("\\", "/"))
-            to_png(os.path.join(root, file))
-
-
-if __name__ == "__main__":
-    home_dir = os.path.expanduser('~')
-
-    resize(f"{home_dir}/Downloads/goat.jpg", 1600, 1600)
-    resize(f"{home_dir}/Downloads/giraffes", 44, 66)
-
-    scale(f"{home_dir}/Downloads/fox.png", 2.5, 3.3)
-    scale(f"{home_dir}/Downloads/cats", 2, 2)
-
-    to_png(f"{home_dir}/Downloads/chicken.jpg")
-    to_png(f"{home_dir}/Downloads/koalas")
+        dir_action(path, to_png_single)
